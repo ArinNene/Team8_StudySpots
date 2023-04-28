f120bdf56c0e6d44d15538047a26e60f3e43c069
Anika Shrivastava <anikashr@usc.edu>
2023-04-28 00:08:50 -0700

    pushing my-reviews

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -5,12 +5,14 @@ public class Review {
 	int userId;
 	int rating;
 	String locationReview;
+	String locationName;
 	
-	public Review(int i, int l, int u, int rating, String r) {
+	public Review(int i, int l, int u, int rating, String r, String name) {
 		id = i;
 		locationId = l;
 		userId = u;
 		this.rating = rating;
 		locationReview = r;
+		locationName = name;
 	}
 }
--- src/main/webapp/my-reviews.js
+++ src/main/webapp/my-reviews.js
@@ -1,15 +1,46 @@
-async function getMyReviews() {
-	//fetch details
-	
-	let url = new URL(window.location.origin + "/Location.java");
-	const params = {
-		method: "USER_REVIEWS",
-		userId: sessionStorage.getItem("user_id")
-	}
-	url.search = new URLSearchParams(params).toString();
-	
-	const response = await fetch(url);
-	const reviews = response.json();
-	return reviews;
-	
-}
+window.onload = function() {
+  var url = window.location.origin + "/Location.java";
+  var data = {
+    userId: sessionStorage.getItem("user_id")
+  };
+  fetch(url, {
+    method: 'POST', // Changed to a valid HTTP method
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    if (data.length == 0) {
+      alert("You have no reviews!");
+    } else {
+      data.forEach(function(review) {
+        var card = document.createElement('div');
+        card.classList.add('card');
+
+        var row1 = document.createElement('div');
+        row1.classList.add('row1');
+        card.appendChild(row1);
+
+        // Add the event details to the card
+        var name = document.createElement('h4');
+        name.textContent = review.location_name; // Fixed variable name
+        row1.appendChild(name);
+
+        var rating = document.createElement('p');
+        rating.textContent = review.rating; // Fixed variable name
+        row1.appendChild(rating);
+
+        var row2 = document.createElement('div');
+        row2.classList.add('row2');
+        card.appendChild(row2);
+
+        var reviewText = document.createElement('p');
+        reviewText.textContent = review.location_review; // Fixed variable name
+        row2.appendChild(reviewText);
+      });
+    }
+  });
+};
